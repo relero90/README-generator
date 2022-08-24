663777b4acd22843b9c6b0c0a49df0cbd35dcc0a
relero90 <relero90@gmail.com>
2022-08-24 16:33:35 -0600

    created user prompting questions in index.js

--- index.js
+++ index.js
@@ -1,10 +1,96 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Describe the project. What does it do? Why is it useful?",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "How can a user install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "How can this project be used?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "How can others contribute to this project?",
+      name: "tocontribute",
+    },
+    {
+      type: "input",
+      message: "How can this project be tested?",
+      name: "testing",
+    },
+    {
+      type: "list",
+      message: "Which license is your project covered under?",
+      choices: [
+        "APM",
+        "AUR",
+        "Bower",
+        "Cocoapods",
+        "Conda",
+        "CPAN",
+        "CRAN/METACRAN",
+        "Crates.io",
+        "CTAN",
+        "DUB",
+        "Eclipse Marketplace",
+        "GitHub",
+        "GitLab",
+        "GitLab (self-managed)",
+        "Greasy Fork",
+        "Hex.pm",
+        "NPM",
+        "Ore",
+        "Packagist",
+        "Packagist (custom server)",
+        "PyPI",
+        "REUSE Compliance",
+        "Weblate Component",
+      ],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "How can you be reached for further questions?",
+      name: "tocontact",
+    },
+  ])
+  .then((data) =>
+    fs.writeFile("./README-output/README.md", writeToREADME(data), (error) =>
+      error ? console.error(err) : console.log("Success!")
+    )
+  );
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToREADME(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
